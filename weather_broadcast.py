3b9edb0051b725207fdd0a266b598c96fa809cb0
dmsgavrilov <65427917+dmsgavrilov@users.noreply.github.com>
2020-11-24 00:03:54 +0300

    Update weather_broadcast.py

--- weather_broadcast.py
+++ weather_broadcast.py
@@ -4,10 +4,11 @@ from bs4 import BeautifulSoup
 
 _URL = "https://yandex.ru/pogoda/"
 _TOWN = r'Прогноз погоды (во|в) (\w+)\b'
-_TEMP = r"Текущая температура[+-]*(\d+)°"
-_FEEL_LIKE = r"Ощущается как[+-]*(\d+)°"
+_TEMP = r"Текущая температура[+−]*(\d+)°"
+_FEEL_LIKE = r"Ощущается как[+−]*(\d+)°"
 _EXPECTED = r"картеВ (.)*Показать"
 _WEATHER = r"°((\w)+( )*)*Ощущается"
+_WIND = r'\d+,*\d+ м/с, \w{2}'
 
 class WeatherBroadcast:
     town = "Moscow"
@@ -18,18 +19,19 @@ class WeatherBroadcast:
         req = requests.get(_URL + self.town)
         info = BeautifulSoup(req.text, "html.parser").get_text()
         get_town = re.search(_TOWN, info).group() + ':'
-        get_temperature = re.search(_TEMP, info).group().replace('+', ': +')
-        get_feeledtemperature = re.search(_FEEL_LIKE, info).group().replace('+', ' +')
+        get_temperature = re.search(_TEMP, info).group().replace('+', ': +').replace('−', ': −')
+        get_felt_temperature = re.search(_FEEL_LIKE, info).group().replace('+', ' +').replace('−', ': −')
         get_weather = re.search(_WEATHER, info).group().replace('°', '').replace('Ощущается', '')
+        get_wind = 'Ветер: ' + re.search(_WIND, info).group()
         try:
             get_expected = re.search(_EXPECTED, info).group().replace("картеВ", "В").replace("Показать", "")
         except:
             get_expected = ""
-        return "\n".join([get_town, get_temperature, get_feeledtemperature, get_weather, get_expected])
+        return "\n".join([get_town, get_temperature, get_felt_temperature,  get_weather, get_wind, get_expected])
 
     def change_town(self, Newtown):
         self.town = Newtown
 
 if __name__ == "__main__":
-    req = requests.get(_URL + "Moscow")
+    req = requests.get(_URL + "Novosibirsk")
     info = BeautifulSoup(req.text, "html.parser").get_text()
